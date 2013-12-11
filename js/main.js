c57f9bf6c31852f49e1c95d995f749f562238c27
MakerSquare Student <students@themakersquare.com>
2013-12-11 11:47:40 -0600

    For loop

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-alert('hi');
+var itemTemplate = $('#templates .item').html();
+
+
+// var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', item: 0.15 }););
+// $('#store').append(newItemHtml);
+
+var items = [
+  { name: 'Gum', price: 0.20 },
+  { name: 'Peppermint', price: 0.50 },
+  { name: 'Pretzel', price: 0.25 }
+]
+
+for (var x = 0; x < items.length; x ++ ) {
+      var item =  items[x];
+      var newItemTemplate = $.render(itemTemplate, item);
+      $('#store').append(newItemTemplate);
+    }
+
+
+
